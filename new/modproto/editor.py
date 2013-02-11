837f9defb6c26aded0ea32e0537487bc795b0b07
Tarmo Knuutinen <tarmo.knuutinen@gmail.com>
2013-02-11 20:43:20 +0200

    added locations and location saving
    added locations and location saving from old repo

--- new/modproto/editor.py
+++ new/modproto/editor.py
@@ -4,13 +4,14 @@ Created on Jan 8, 2013
 @author: TarmoK
 '''
 
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from views import createTemplateData
 from utilities import wrapTags, wrapTagsWithInnerData
 from django.contrib.auth.decorators import login_required
-from proto.models import DBLocation, DBCondition, DBEvent, DBFeature
+from proto.models import DBLocation, DBCondition, DBEvent, DBFeature, UserProfile, DBGame
 
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 
 class NullModule(object):
     def __init__(self):
@@ -20,11 +21,51 @@ class NullModule(object):
         self.id = 0
         self.adminname = ""
         
+def get_empty_names(fields):
+    empties = []
+    for key, value in fields.iteritems():
+        if value == None or value == "":
+            empties.append(key)
+    return ", ".join(empties)
+
+def editor_error(request, template, message):
+    return render_to_response(template, createTemplateData({"location": NullModule(), "message" : message}), context_instance=RequestContext(request))
+
+def location_save(request):
+    editorname = request.POST.get('editorname', None)
+    name = request.POST.get('name', None)
+    ftdesc = request.POST.get('ftdesc', None) or None
+    desc = request.POST.get('desc', None)
+    notes = request.POST.get('notes', None) or None
+    if not UserProfile.objects.get(user=request.user).disabled:
+        if editorname and name and desc:
+            #TODO: allow custom games
+            game = DBGame.objects.get(name="default")
+            if not DBLocation.objects.filter(name=name, game=game).exists():
+                newloc = DBLocation(creator=request.user, game=game, adminname=editorname, name=name, ftdesc=ftdesc, desc=desc, notes=notes)
+                newloc.save()
+                return redirect("/proto/editor/location/" + str(newloc.id))
+            else:
+                return editor_error(request, "location.html", "Location with that name already exists for the specified game.")
+        else:
+            return editor_error(request, "location.html", "Please input valid values to the fields: " + get_empty_names({"Name" : name, "Editor name" : editorname, "Description" : desc}))
+    else:
+        return editor_error(request, "location.html", "Error: User account disabled.")
+
 @login_required  
-def location(request):
+def location(request, id=None, message=None):
     if request.POST:
-        pass
-    return render_to_response("location.html", createTemplateData({"location": NullModule()}), context_instance=RequestContext(request))
+        return location_save(request)
+    if id:
+        if id.isdigit() and DBLocation.objects.filter(id=int(id)).exists():
+            oldlocation = DBLocation.objects.filter(id=int(id))[0]
+            if oldlocation.is_editable(request.user):
+                return render_to_response("location.html", createTemplateData({"location": oldlocation, "message" : message}), context_instance=RequestContext(request))
+            else:
+                return editor_error(request, "location.html", "Not editable by you.")
+        else:
+            return editor_error(request, "location.html", "Error: No such Location.")
+    return render_to_response("location.html", createTemplateData({"location": NullModule(), "message" : message}), context_instance=RequestContext(request))
 
 @login_required
 def editor_main(request):
