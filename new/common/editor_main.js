d291addadc4b6d0f4f49732092a645c289d62d69
Tarmo Knuutinen <tarmo.knuutinen@gmail.com>
2013-02-23 20:05:11 +0200

    added helpbox to editor modules

--- new/common/editor_main.js
+++ new/common/editor_main.js
@@ -3,4 +3,10 @@ $(document).ready(function(){
     $("#navigation li").each(function(){ 
         setHandHover($(this));
     });
+    
+    $(".helpbox").hide();
+    $(".module").focus(function(){
+        $(".helpbox").show();
+        $(".helpbox").text($(this).next().html());
+    });
 });
