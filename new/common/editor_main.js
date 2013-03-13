94a339893d0b656729bd978a400cc4ac8c14766a
Tarmo Knuutinen <tarmo.knuutinen@gmail.com>
2013-03-13 15:45:28 +0200

    refactored editor templates, refactored help
    help now appears on-hover, but can be disabled with a button. disabling
    the help box is saved into a cookie.

--- new/common/editor_main.js
+++ new/common/editor_main.js
@@ -4,9 +4,32 @@ $(document).ready(function(){
         setHandHover($(this));
     });
     
-    $(".helpbox").hide();
-    $(".module").focus(function(){
-        $(".helpbox").show();
-        $(".helpbox").text($(this).next().html());
+    var helpbox = $(".helpbox");
+    $(".helpsubject").hover(function(){
+        helpbox.text($(this).find(".element_helptext").html());
+    });
+    
+    var hidden = false;
+    var cookiename = "modme_editor_help_show";
+    
+    if(parseInt(readCookie(cookiename), 10) == 0){
+        hidden = true;
+        helpbox.hide();
+    }
+    
+    $(".hidehelpbutton").click(function(e){
+        e.preventDefault();
+        if(!hidden){
+            helpbox.hide();
+            eraseCookie(cookiename);
+            createCookie(cookiename, "0", 1);
+            hidden = true;
+        }
+        else{
+            helpbox.show();
+            eraseCookie(cookiename);
+            createCookie(cookiename, "1", 1);
+            hidden = false;
+        }
     });
 });
